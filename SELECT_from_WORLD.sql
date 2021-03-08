39926a9267cb73dd4320fac99fddbd3ceda75435
uwadonat <uwadonat@gmail.com>
2021-03-08 14:13:49 +0200

    ADD SQL FOR SELECT_from_WORLD questions

--- SELECT_from_WORLD.sql
+++ SELECT_from_WORLD.sql
@@ -24,3 +24,42 @@ SELECT name, population/1000000
 
  SELECT name, population FROM world
  WHERE name IN ('France','Germany','Italy')
+
+ --6. Show the countries which have a name that includes the word 'United'
+
+ SELECT name FROM world WHERE name REGEXP 'United'
+
+ --7. Show the countries that are big by area or big by population. Show name, population and area.
+
+ SELECT name, population, area FROM world WHERE area > 3000000 OR population > 250000000
+
+ --8. Show the countries that are big by area (more than 3 million) or big by population (more than 250 million) but not both.
+
+ SELECT name, population, area FROM world WHERE area > 3000000 XOR population > 250000000
+
+ --9. Show the name and population in millions and the GDP in billions for the countries of the continent 'South America'. Use the ROUND function to show the values to two decimal places. 
+
+ SELECT name, ROUND(population/1000000, 2), ROUND(gdp/1000000000, 2) FROM world WHERE continent = 'South America'
+
+ --10. Show the name and per-capita GDP for those countries with a GDP of at least one trillion (1000000000000; that is 12 zeros). Round this value to the nearest 1000
+
+ SELECT name, ROUND(gdp/population, -3) FROM world WHERE gdp >= 1000000000000
+ 
+ --11. Show the name and capital where the name and the capital have the same number of characters.
+
+ SELECT name, capital FROM world where LENGTH(name) = LENGTH(capital)
+
+ -- 12. Show the name and the capital where the first letters of each match. Don't include countries where the name and the capital are the same word.
+
+ SELECT name, capital FROM world where LEFT(name, 1) = LEFT(capital, 1) AND name <> capital
+
+ --13. Find the country that has all the vowels and no spaces in its name.
+
+ SELECT  name
+FROM world
+WHERE name LIKE '%a%' 
+AND name LIKE '%e%' 
+AND name LIKE '%i%' 
+AND name LIKE '%o%' 
+AND name LIKE '%u%' 
+AND name NOT LIKE '% %' 
