2605ba4cb38ac78f7ce4fc13da2505b85b5af4f8
uwadonat <uwadonat@gmail.com>
2021-03-08 11:39:03 +0200

    ADD SQL FOR SELECT_basics

---
+++ SELECT_from_WORLD.sql
@@ -0,0 +1,26 @@
+-- 1. show the name, continent and population of all countries. 
+
+SELECT name, continent, population FROM world
+
+--2.  Show the name for the countries that have a population of at least 200 million.
+
+SELECT name
+  FROM world
+ WHERE population >= 200000000;
+
+ --3. Give the name and the per capita GDP for those countries with a population of at least 200 million. 
+
+ SELECT name, gdp/population
+  FROM world
+ WHERE population >= 200000000;
+
+--4. Show the name and population in millions for the countries of the continent 'South America'. 
+
+SELECT name, population/1000000
+  FROM world
+ WHERE continent = 'South America'
+
+ -- 5. Show the name and population for France, Germany, Italy
+
+ SELECT name, population FROM world
+ WHERE name IN ('France','Germany','Italy')
